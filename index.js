5bf1b4c9f7f7184759c6ff1cc47338f04f8ea722
Marvin Siri <marvinsiri123@gmail.com>
2023-08-01 15:34:23 -0400

    moving functionality

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let square = document.getElementById("square");
+let square = document.getElementById('square');
 console.log(square.style.left, square.style.top);
 
-document.addEventListener("keydown", function(e) {
-  console.log(e.key)
+document.addEventListener('keydown', function (e) {
+  let x = parseInt(square.style.left);
+  let y = parseInt(square.style.top);
+
+  if (e.key === 'ArrowLeft') {
+    square.style.left = `${x - 10}px`;
+  } else if (e.key === 'ArrowRight') {
+    square.style.left = `${x + 10}px`;
+  } else if (e.key === 'ArrowUp') {
+    square.style.top = `${y - 10}px`;
+  } else if (e.key === 'ArrowDown') {
+    square.style.top = `${y + 10}px`;
+  }
 });
